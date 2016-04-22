b30768c39721c7a7b65d21543b6fb0a691c46eb4
张磊 <zhanglei@zhangleideMacBook-Pro.local>
2016-04-22 17:52:31 +0800

    初始化环境

---
+++ Douban/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Douban
+//
+//  Created by 张磊 on 16/4/22.
+//  Copyright © 2016年 张磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Douban/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Douban
+//
+//  Created by 张磊 on 16/4/22.
+//  Copyright © 2016年 张磊. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
